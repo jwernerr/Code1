4063f887409f1f39e218ba35991afa70ce606507
jwernerr <jenny.w0400@gmail.com>
2024-06-20 14:03:41 +0200

    mmmmmmm

--- L04_JS/FirstCode.js
+++ L04_JS/FirstCode.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log("hello world from ts test");
+let x = 69;
+x += 1;
+console.log(x);
--- L04_JS/FirstCode.ts
+++ L04_JS/FirstCode.ts
@@ -1,1 +1,4 @@
 console.log("hello world from ts test");
+let x=69;
+x+=1;
+console.log(x)
