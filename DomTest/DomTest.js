dd90f62143be9b7054332e79068ddd5e11fd342f
jwernerr <jenny.w0400@gmail.com>
2024-06-25 11:01:03 +0200

    newspan

--- DomTest/DomTest.js
+++ DomTest/DomTest.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log("hello");
+let newSpan = document.createElement("span");
+newSpan.textContent = "gggggg";
+document.body.appendChild(newSpan);
--- DomTest/DomTest.ts
+++ DomTest/DomTest.ts
@@ -1,1 +1,4 @@
 console.log("hello")
+let newSpan:HTMLElement=document.createElement("span");
+newSpan.textContent="gggggg";
+document.body.appendChild(newSpan);
