86d5e308c38d9482a493da1e062d1f6e2c2bbcf1
jwernerr <jenny.w0400@gmail.com>
2024-06-25 11:56:04 +0200

    ajniajknjak

--- L05_DomManipulation/L05_RanDOM/RanDOM.js
+++ L05_DomManipulation/L05_RanDOM/RanDOM.js
@@ -1,1 +1,6 @@
 "use strict";
+const wordsarray = ["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
+let index = Math.floor(Math.random() * 8);
+let newSpan = document.createElement("span");
+newSpan.textContent = wordsarray[index];
+document.body.appendChild(newSpan);
