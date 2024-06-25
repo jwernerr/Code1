98dab2bca9d80fd6fb8719f90ca9f7fd49dda1cb
jwernerr <jenny.w0400@gmail.com>
2024-06-25 11:41:40 +0200

    domtest

--- DomTest/DomTest.js
+++ DomTest/DomTest.js
@@ -3,3 +3,13 @@ console.log("hello");
 let newSpan = document.createElement("span");
 newSpan.textContent = "gggggg";
 document.body.appendChild(newSpan);
+newSpan.style.backgroundColor = "red";
+let i = 0;
+do {
+    i++;
+    console.log(i);
+    let newSpan = document.createElement("span");
+    newSpan.textContent = "gggggg";
+    document.body.appendChild(newSpan);
+    newSpan.style.backgroundColor = "red";
+} while (i < 10);
--- DomTest/DomTest.ts
+++ DomTest/DomTest.ts
@@ -2,3 +2,14 @@ console.log("hello")
 let newSpan:HTMLElement=document.createElement("span");
 newSpan.textContent="gggggg";
 document.body.appendChild(newSpan);
+newSpan.style.backgroundColor="red";
+
+let i:number=0
+do {
+    i++
+    console.log(i)
+    let newSpan:HTMLElement=document.createElement("span");
+    newSpan.textContent="gggggg";
+    document.body.appendChild(newSpan);
+    newSpan.style.backgroundColor="red";
+} while (i<10)
