998683e8c8491e0bcc9397e042dd48309cf975b5
jwernerr <jenny.w0400@gmail.com>
2024-06-25 14:11:44 +0200

    commit2

--- L05_DomManipulation/L05_RanDOM/RanDOM.js
+++ L05_DomManipulation/L05_RanDOM/RanDOM.js
@@ -1,6 +1,27 @@
 "use strict";
-const wordsarray = ["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
-let index = Math.floor(Math.random() * 8);
-let newSpan = document.createElement("span");
-document.body.appendChild(newSpan);
-newSpan.textContent = wordsarray[index];
+//const wordsArray:string[]= ["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
+//let iNdex:number= Math.floor(Math.random() * 8);
+//let newSpan = document.createElement("span");
+//document.body.appendChild(newSpan);
+//newSpan.textContent = wordsArray[iNdex];
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function createRandomElement() {
+    const element = document.createElement('div');
+    element.style.position = 'absolute';
+    element.style.width = `${getRandomInt(50, 100)}px`;
+    element.style.height = `${getRandomInt(50, 100)}px`;
+    element.style.left = `${getRandomInt(0, window.innerWidth - 100)}px`;
+    element.style.top = `${getRandomInt(0, window.innerHeight - 100)}px`;
+    element.innerText = 'Hello!';
+    return element;
+}
--- L05_DomManipulation/L05_RanDOM/RanDOM.ts
+++ L05_DomManipulation/L05_RanDOM/RanDOM.ts
@@ -1,7 +1,31 @@
-const wordsarray=["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
+//const wordsArray:string[]= ["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
+//let iNdex:number= Math.floor(Math.random() * 8);
+//let newSpan = document.createElement("span");
+//document.body.appendChild(newSpan);
+//newSpan.textContent = wordsArray[iNdex];
+function getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function createRandomElement():HTMLElement{
+    const element = document.createElement('div');
 
-let index:number=Math.floor(Math.random() * 8);
-let newSpan:HTMLElement=document.createElement("span");
+    element.style.position = 'absolute';
+    element.style.width = `${getRandomInt(50, 100)}px`;
+    element.style.height =`${getRandomInt(50, 100)}px`;
 
-document.body.appendChild(newSpan);
-newSpan.textContent=wordsarray[index];
+    element.style.left = `${getRandomInt(0, window.innerWidth - 100)}px`;
+    element.style.top = `${getRandomInt(0, window.innerHeight - 100)}px`;
+
+    element.innerText = 'Hello!';
+
+    return element;
+  }
