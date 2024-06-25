c71ab4579421d80f4e8ed6f34faeb839c7cc6270
jwernerr <jenny.w0400@gmail.com>
2024-06-25 11:57:47 +0200

    comit

--- L05_DomManipulation/L05_RanDOM/RanDOM.js
+++ L05_DomManipulation/L05_RanDOM/RanDOM.js
@@ -2,5 +2,5 @@
 const wordsarray = ["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
 let index = Math.floor(Math.random() * 8);
 let newSpan = document.createElement("span");
-newSpan.textContent = wordsarray[index];
 document.body.appendChild(newSpan);
+newSpan.textContent = wordsarray[index];
--- L05_DomManipulation/L05_RanDOM/RanDOM.ts
+++ L05_DomManipulation/L05_RanDOM/RanDOM.ts
@@ -1,6 +1,7 @@
-const wordsarray=["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"]
+const wordsarray=["rat", "cheese", "mouse", "mousetrap", "tail", "feet", "whiskers", "fur"];
 
-let index:number=Math.floor(Math.random() * 8)
+let index:number=Math.floor(Math.random() * 8);
 let newSpan:HTMLElement=document.createElement("span");
-newSpan.textContent=wordsarray[index]
+
 document.body.appendChild(newSpan);
+newSpan.textContent=wordsarray[index];
