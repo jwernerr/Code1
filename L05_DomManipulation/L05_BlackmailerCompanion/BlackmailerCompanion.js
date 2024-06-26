9034e380133112af9a51462b3eac95da75c80068
jwernerr <jenny.w0400@gmail.com>
2024-06-26 13:12:48 +0200

    kkkk

--- L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.js
+++ L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.js
@@ -4,11 +4,20 @@ window.addEventListener("load", handleLoad);
 function handleLoad(_event) {
     let mail = document.querySelector("div#mail");
     mail.addEventListener("click", placeletter);
-    document.addEventListener("keydown", chooseletter);
+    document.addEventListener("keydown", choosecharacter);
 }
 function placeletter(_event) {
-    console.log(_event);
+    // console.log(_event);
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    let mail = _event.target;
+    let letter = document.createElement("span");
+    mail.appendChild(letter);
+    letter.textContent = chosencharacter;
+    letter.style.left = x + "px";
+    letter.style.top = y + "px";
 }
-function chooseletter(_event) {
-    console.log(_event);
+function choosecharacter(_event) {
+    // console.log(_event);
+    chosencharacter = _event.key;
 }
--- L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.ts
+++ L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.ts
@@ -4,13 +4,24 @@ window.addEventListener("load",handleLoad);
 function handleLoad(_event:Event):void{
     let mail:HTMLElement=document.querySelector("div#mail")!;
     mail.addEventListener("click",placeletter);
-    document.addEventListener("keydown",chooseletter);
+    document.addEventListener("keydown",choosecharacter);
 }
 
 function placeletter(_event:MouseEvent):void{
-    console.log(_event);
+    // console.log(_event);
+    let x:number=_event.offsetX;
+    let y:number=_event.offsetY;
+
+    let mail:HTMLElement=<HTMLElement>_event.target;
+    let letter:HTMLSpanElement=document.createElement("span");
+    mail.appendChild(letter);
+
+    letter.textContent=chosencharacter;
+    letter.style.left=x+"px";
+    letter.style.top=y+"px";
 }
 
-function chooseletter(_event:KeyboardEvent):void{
-    console.log(_event);
+function choosecharacter(_event:KeyboardEvent):void{
+    // console.log(_event);
+    chosencharacter=_event.key;
 }
