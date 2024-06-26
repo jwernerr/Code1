a417a5599b3334a23b309dd4cd670f7097bd60ca
jwernerr <jenny.w0400@gmail.com>
2024-06-26 12:43:27 +0200

    create

---
+++ L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.js
@@ -0,0 +1,14 @@
+"use strict";
+let chosencharacter;
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    let mail = document.querySelector("div#mail");
+    mail.addEventListener("click", placeletter);
+    document.addEventListener("keydown", chooseletter);
+}
+function placeletter(_event) {
+    console.log(_event);
+}
+function chooseletter(_event) {
+    console.log(_event);
+}
---
+++ L05_DomManipulation/L05_BlackmailerCompanion/BlackmailerCompanion.ts
@@ -0,0 +1,16 @@
+let chosencharacter:string;
+window.addEventListener("load",handleLoad);
+
+function handleLoad(_event:Event):void{
+    let mail:HTMLElement=document.querySelector("div#mail")!;
+    mail.addEventListener("click",placeletter);
+    document.addEventListener("keydown",chooseletter);
+}
+
+function placeletter(_event:MouseEvent):void{
+    console.log(_event);
+}
+
+function chooseletter(_event:KeyboardEvent):void{
+    console.log(_event);
+}
